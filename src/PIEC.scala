9331f33819aacd6316ae4c7d7aba48bb4d299c25
cgvlas <cvlas@iit.demokritos.gr>
2018-07-24 18:08:02 +0300

    This is the first commit that involves Scala code.

---
+++ src/PIEC.scala
@@ -0,0 +1,17 @@
+package piec
+
+/**
+  * @author Christos G. Vlassopoulos (cvlas@iit.demokritos.gr)
+  *
+  * 2018-07-24
+  */
+
+class PIEC
+{
+    //var probList: Array = <Value>
+    var threshold: Double = 0.5
+
+    def piec(alpha: Array, tau: Double): Unit = {
+
+    }
+}
