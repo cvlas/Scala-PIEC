a879013add0449d3c07eba5968313fac3131f805
cgvlas <cvlas@iit.demokritos.gr>
2018-07-27 18:51:04 +0300

    Added visio figure for PIEC. Minor code changes.

--- src/piec/PIEC.scala
+++ src/piec/PIEC.scala
@@ -10,9 +10,17 @@ object PIEC extends App {
 
   def piec(a: Array[Double], t: Double): Unit = {
 
-    val l = new Array[Double](a.size)
-    for (i <- 0 until a.size) l(i) = a(i) - t
-    for (i <- 0 until a.size) println(l(i))
+    val l = new Array[Double](a.length)
+    val prefix = new Array[Double](a.length)
+    val dp = new Array[Double](a.length)
+
+    for (i <- a.indices) {
+      l(i) = a(i) - t
+
+      // TODO: Build prefix and dp arrays
+    }
+
+    // TODO: Print all arrays
   }
 
   val aa = Array(0.8, 0.6, 0.1, 0.0, 0.9, 0.6, 0.8, 0.5, 0.2, 0.1, 0.6)
