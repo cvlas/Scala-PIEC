cde86467c0e282e3f7e78af42c2a388f9fb7b4e1
Christos Vlassopoulos <christos.vlas@gmail.com>
2018-07-29 18:40:56 +0300

    Added paper to project. Wrote some more lines of code.

--- src/piec/PIEC.scala
+++ src/piec/PIEC.scala
@@ -8,21 +8,33 @@ package piec
 
 object PIEC extends App {
 
-  def piec(a: Array[Double], t: Double): Unit = {
+	def piec(a: Array[Double], t: Double): Unit = {
 
-    val l = new Array[Double](a.length)
-    val prefix = new Array[Double](a.length)
-    val dp = new Array[Double](a.length)
+		val l = new Array[Double](a.length)
+		val prefix = new Array[Double](a.length)
+		val dp = new Array[Double](a.length)
 
-    for (i <- a.indices) {
-      l(i) = a(i) - t
+		for (i <- a.indices) {
+			l(i) = a(i) - t
+			
+			prefix(i) = l(i)
+			for (j <- 0 until i) prefix(i) += l(j)
+		}
+	
+		for (k <- (a.indices).reverse) {
+			if (k == (a.indices).last) dp(k) = prefix(k)
+			else {
+				if (prefix(k) > dp(k+1)) dp(k) = prefix(k)
+				else dp(k) = dp(k+1)
+			}
+		}
 
-      // TODO: Build prefix and dp arrays
-    }
+		println(a)
+		println(l)
+		println(prefix)
+		println(dp)
+	}
 
-    // TODO: Print all arrays
-  }
-
-  val aa = Array(0.8, 0.6, 0.1, 0.0, 0.9, 0.6, 0.8, 0.5, 0.2, 0.1, 0.6)
-  piec(aa, 0.5)
+	val aa = Array(0.8, 0.6, 0.1, 0.0, 0.9, 0.6, 0.8, 0.5, 0.2, 0.1, 0.6)
+	piec(aa, 0.5)
 }
