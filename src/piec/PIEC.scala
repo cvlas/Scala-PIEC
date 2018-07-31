10cfd352d0ed75e188150ba0eae64243f110146a
cgvlas <cvlas@iit.demokritos.gr>
2018-07-31 15:08:20 +0300

    Code correctly constructs arrays a, l, prefix and dp

--- src/piec/PIEC.scala
+++ src/piec/PIEC.scala
@@ -6,7 +6,8 @@ package piec
   * 2018-07-27
   */
 
-object PIEC extends App {
+object PIEC extends App
+{
 
 	def piec(a: Array[Double], t: Double): Unit = {
 
@@ -14,27 +15,41 @@ object PIEC extends App {
 		val prefix = new Array[Double](a.length)
 		val dp = new Array[Double](a.length)
 
-		for (i <- a.indices) {
-			l(i) = a(i) - t
-			
+		for (i <- a.indices)
+        {
+			// Construct l array, based on a
+			// TODO: Check for possibly better rounding options
+			l(i) = BigDecimal(a(i) - t).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+			// Construct prefix array, based on l
 			prefix(i) = l(i)
 			for (j <- 0 until i) prefix(i) += l(j)
+			// TODO: Check for possibly better rounding options
+			prefix(i) = BigDecimal(prefix(i)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
 		}
 	
-		for (k <- (a.indices).reverse) {
-			if (k == (a.indices).last) dp(k) = prefix(k)
-			else {
-				if (prefix(k) > dp(k+1)) dp(k) = prefix(k)
+		for (k <- a.indices.reverse)                        // Construct dp array, backwards, based on prefix
+        {
+			if (k == a.indices.last)
+            {
+                dp(k) = prefix(k)
+            }
+			else
+            {
+				if (prefix(k) > dp(k+1))
+                {
+                    dp(k) = prefix(k)
+                }
 				else dp(k) = dp(k+1)
 			}
 		}
 
-		println(a)
-		println(l)
-		println(prefix)
-		println(dp)
+		println(a.deep.mkString(", "))
+		println(l.deep.mkString(", "))
+		println(prefix.deep.mkString(", "))
+		println(dp.deep.mkString(", "))
 	}
 
-	val aa = Array(0.8, 0.6, 0.1, 0.0, 0.9, 0.6, 0.8, 0.5, 0.2, 0.1, 0.6)
+	val aa = Array(0.0, 0.5, 0.7, 0.9, 0.4, 0.1, 0.0, 0.0, 0.5, 1.0)
 	piec(aa, 0.5)
 }
