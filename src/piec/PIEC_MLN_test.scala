d7a10908c901ac5c23a6c3a04aa58df89fda4711
Christos Vlassopoulos <cvlas@di.uoa.gr>
2019-01-09 03:40:07 +0200

    Updated MLN test source code

--- src/piec/PIEC_MLN_test.scala
+++ src/piec/PIEC_MLN_test.scala
@@ -9,14 +9,14 @@ import scala.io.Source
 /**
   * @author Christos G. Vlassopoulos (cvlas@iit.demokritos.gr)
   *
-  *         2018-07-27
+  *         2019-01-07
   */
 
 object PIEC_MLN_test extends App
 {
-    def formatGround(x: List[(Int, Int)]): Array[Int] =
+    def formatGround(x: List[(Int, Int)], n: Int): Array[Int] =
     {
-        var start = Array.fill[Int](25170)(0)
+        val start = Array.fill[Int](n)(0)
 
         for (i <- x)
         {
@@ -80,13 +80,13 @@ object PIEC_MLN_test extends App
     {
         if (tuples.isEmpty)
         {
-            Array.fill[Int](25170)(0)
+            Array.fill[Int](prefix.length)(0)
         }
         else
         {
             if (tuples.length == 1)
             {
-                formatGround(tuples)
+                formatGround(tuples, prefix.length)
             }
             else
             {
@@ -135,16 +135,18 @@ object PIEC_MLN_test extends App
 
                 overlap += currentInterval
 
-                formatGround(overlap.toList)
+                formatGround(overlap.toList, prefix.length)
             }
         }
     }
 
-    def piec(inputArray: Array[Double], threshold: Double): Array[Int] = {
+    def piec(inputArray: Array[Double], threshold: Double): Array[Int] =
+    {
         val prefixInput = new Array[Double](inputArray.length)
         prefixInput(0) = inputArray(0)
 
-        for (i <- 1 until inputArray.length) {
+        for (i <- 1 until inputArray.length)
+        {
             prefixInput(i) = BigDecimal(prefixInput(i - 1) + inputArray(i)).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
         }
 
@@ -152,19 +154,22 @@ object PIEC_MLN_test extends App
         val dp = new Array[Double](inputArray.length)
         var result = ListBuffer[(Int, Int)]()
 
-        for (x <- inputArray) {
+        for (x <- inputArray)
+        {
             inputArray(inputArray.indexOf(x)) = BigDecimal(x - threshold).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
         }
 
         prefix(0) = BigDecimal(inputArray(0)).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
 
-        for (i <- 1 until inputArray.length) {
+        for (i <- 1 until inputArray.length)
+        {
             prefix(i) = BigDecimal(prefix(i - 1) + inputArray(i)).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
         }
 
         dp(inputArray.length - 1) = prefix(inputArray.length - 1)
 
-        for (i <- inputArray.length - 2 until -1 by -1) {
+        for (i <- inputArray.length - 2 until -1 by -1)
+        {
             dp(i) = BigDecimal(Seq(dp(i + 1), prefix(i)).max).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
         }
 
@@ -173,13 +178,15 @@ object PIEC_MLN_test extends App
         var end = 0
         var flag1 = false
 
-        while (end < inputArray.length && start < inputArray.length) {
+        while (end < inputArray.length && start < inputArray.length)
+        {
             if (start == 0)
                 dprange = dp(end)
             else
                 dprange = BigDecimal(dp(end) - prefix(start - 1)).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
 
-            if (dprange >= 0) {
+            if (dprange >= 0)
+            {
                 if (end == inputArray.length - 1 && start < end)
                     result += ((start, end))
                 if (end == inputArray.length - 1 && start == end && inputArray(start) > 0)
@@ -188,7 +195,8 @@ object PIEC_MLN_test extends App
                 flag1 = true
                 end = end + 1
             }
-            else {
+            else
+            {
                 if (start < end && flag1)
                     result += ((start, end - 1))
                 if (start == end && inputArray(start) > 0)
@@ -202,23 +210,34 @@ object PIEC_MLN_test extends App
         getCredible(result.toList, prefixInput)
     }
 
-    def evaluateResults(g: Array[Int], p: Array[Int]): (Int, Int, Int, Int) = {
+    def evaluateResults(g: Array[Int], p: Array[Int]): (Int, Int, Int, Int) =
+    {
         var (tn, fp, fn, tp) = (0, 0, 0, 0)
 
-        if (g.length == p.length) {
-            //println("OKIE DOKIE1")
+        if (g.length == p.length)
+        {
             val n = g.length
 
             if (n > 0)
             {
-                //println("OKIE DOKIE2")
                 for (i <- 0 until n)
                 {
-                    if (g(i) == 0 && p(i) == 0) tn += 1
-                    if (g(i) == 0 && p(i) == 1) fp += 1
-                    if (g(i) == 1 && p(i) == 0) fn += 1
-                    if (g(i) == 1 && p(i) == 1) tp += 1
-                    //println(s"${p(i)}")
+                    if (g(i) == 0 && p(i) == 0)
+                    {
+                        tn += 1
+                    }
+                    if (g(i) == 0 && p(i) == 1)
+                    {
+                        fp += 1
+                    }
+                    if (g(i) == 1 && p(i) == 0)
+                    {
+                        fn += 1
+                    }
+                    if (g(i) == 1 && p(i) == 1)
+                    {
+                        tp += 1
+                    }
                 }
             }
             else
@@ -242,22 +261,75 @@ object PIEC_MLN_test extends App
 
     for (ltaHome <- oslaHome.listFiles())
     {
-        println(s"Working on: ${ltaHome.getName}")
+        println(s"% ------------------------------------------------------------------------------\n% LONG-TERM ACTIVITY: ${ltaHome.getName}\n% ------------------------------------------------------------------------------\n")
 
         val resultsHomePath = s"${ltaHome.getAbsolutePath}\\results"
-        val annotationHomePath = s"$resultsHomePath\\annotation"
+        // val annotationHomePath = s"$resultsHomePath\\annotation"
 
         val resultsHome = new File(resultsHomePath)
-        val annotationHome = new File(annotationHomePath)
+        // val annotationHome = new File(annotationHomePath)
 
         for (resultFile <- resultsHome.listFiles().filter(x => x.getName.endsWith(".csv")))
         {
-            val prefix = resultFile.getName.split("\\.")(0)
-            val annotationFilePath = s"$annotationHomePath\\$prefix.db"
+            val resultFileNameSplit = resultFile.getName.split("\\.")(0).split("_")
+            val (idA, idB) = (resultFileNameSplit(resultFileNameSplit.length - 1), resultFileNameSplit(resultFileNameSplit.length - 2))
 
-            val annotationFile = new File(annotationFilePath)
+            val lastLine = Source.fromFile(resultFile).getLines().foldLeft(Option.empty[String]) { case (_, line) => Some(line) }
 
-            println(s"RESULTS: ${resultFile.getName}\tANNOTATION: ${annotationFile.getName}")
+            val lastPoint = lastLine match
+            {
+                case Some(value) => value.split(",")(0).toInt
+                case None => 0
+            }
+
+            for (th <- 0.5 to 0.91 by 0.2)
+            {
+                val probabilities = Array.fill(lastPoint + 1)(0.0)
+                val groundTruth = Array.fill(lastPoint + 1)(0)
+
+                for (line <- Source.fromFile(resultFile).getLines())
+                {
+                    val (time, probability, annot) = line.split(",") match
+                    {
+                        case Array(s1, s2, s3) => (s1.toInt, s2.toDouble, s3.toInt)
+                    }
+
+                    probabilities(time) = probability
+                    groundTruth(time) = annot
+                }
+
+                val thround = BigDecimal(th).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+                println(s"Working on ${resultFile.getName} MLN-EC result file, threshold: $thround...")
+
+                val results_m = evaluateResults(groundTruth, mlnec_intervals(probabilities, thround))
+                println(s"MLN-EC: $results_m")
+                val results_p = evaluateResults(groundTruth, piec(probabilities, thround))
+                println(s"PIEC: $results_p\n")
+
+                val prec_mlnec = if ((results_m._4 + results_m._2) == 0) 0
+                                     else BigDecimal(results_m._4 / (results_m._4 + results_m._2).toDouble).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+                val rec_mlnec = if ((results_m._4 + results_m._3) == 0) 0
+                                    else BigDecimal(results_m._4 / (results_m._4 + results_m._3).toDouble).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+                val f1_mlnec = if ((prec_mlnec + rec_mlnec) == 0.0) 0
+                                   else BigDecimal((2 * prec_mlnec * rec_mlnec) / (prec_mlnec + rec_mlnec)).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+                println(s"precision_${ltaHome.getName}_mln-ec : ${prec_mlnec}")
+                println(s"recall_${ltaHome.getName}_mln-ec : ${rec_mlnec}")
+                println(s"f1-score_${ltaHome.getName}_mln-ec : ${f1_mlnec}")
+
+                val prec_piec = if ((results_p._4 + results_p._2) == 0) 0
+                else BigDecimal(results_p._4 / (results_p._4 + results_p._2).toDouble).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+                val rec_piec = if ((results_p._4 + results_p._3) == 0) 0
+                else BigDecimal(results_p._4 / (results_p._4 + results_p._3).toDouble).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+                val f1_piec = if ((prec_piec + rec_piec) == 0.0) 0
+                else BigDecimal((2 * prec_piec * rec_piec) / (prec_piec + rec_piec)).setScale(6, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+                println(s"precision_${ltaHome.getName}_piec : ${prec_piec}")
+                println(s"recall_${ltaHome.getName}_piec : ${rec_piec}")
+                println(s"f1-score_${ltaHome.getName}_piec : ${f1_piec}\n\n")
+            }
+
+            println("% ------------------------------------------------------------------------------\n")
         }
     }
 }
